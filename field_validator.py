fe7110d2a1e4bbafb5cc3f3d2dc12990fe0c806a
Mudassar Gill <mudassarhussain6533@gmail.com>
2026-02-20 22:13:27 -0800

    Add field validator using class method

---
+++ field_validator.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel,Field,field_validator,EmailStr,AnyUrl
+from typing import Optional,Dict,List,Annotated
+
+class patients(BaseModel):
+    name:str
+    age:Annotated[int,Field(gt=0,lt=100,description="Age of the person",examples=["25","30"])]
+    email:str
+def insert_data(patient:patients):
+    print(f"inserting {patient.name} into the database")
+    print(f"inserting {patient.age} into the database")
+    print(f"inserting {patient.email} into the database")
+    print("Data inserted successfully")
+    
+@field_validator("email") 
+@classmethod
+def validate_email(cls,value):
+     domain=['hbl.com','gov.com']
+     domain_name=value.split("@")[-1]
+     if domain_name not in domain:
+         raise ValueError("Invalid email")
+     return value
+   
+patient_info={
+    "name":"John",
+    "age":30,
+    "email":"abc@gmail.com"
+}
+patient_ob1=patients(**patient_info)
+print(patient_ob1)
+insert_data(patient_ob1)
+
+    
+    
